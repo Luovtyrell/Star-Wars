76936c13a900db640a13bc984283b56403c14c1d
Luovtyrell <luciakjhgfds@gmail.com>
2024-08-15 16:06:49 +0200

    Trying to fix Vercel deployment import issue

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-    },
-  },
   css: {
     postcss: './postcss.config.js',
   },
